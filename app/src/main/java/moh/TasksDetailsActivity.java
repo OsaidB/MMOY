91c844df4b8ccc8e13157b92d59f8d17460c3742
MohammadMurrar <1200698@student.birzeit.edu>
2024-02-11 15:40:18 +0200

    Tasks almost finish

---
+++ app/src/main/java/moh/TasksDetailsActivity.java
@@ -0,0 +1,16 @@
+package moh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ps.example.mmoy.R;
+
+public class TasksDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.moh_activity_tasks_details);
+    }
+}
